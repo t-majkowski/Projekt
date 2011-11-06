8d2aebde7e651eb71d84c9de057e64e9884be77f
Tomasz Majkowski <majak_15@o2.pl>
2011-11-06 18:35:28 +0100

    .

---
+++ fortunka/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fortunka::Application.config.secret_token = 'ccef8061d6b8ecd90342f76900955b31dc8a5bfdfc72d8450a0c9357ff47d02ceca769e3a42f6d6aa6a670e6be6830e6e5614868ad659845bf59fd926f99d52f'
---
+++ fortunka/db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+Fortune.create :quotation => 'I hear and i forget. I see and i remember. I do and I understand.'
+Fortune.create :quotation => 'Everything has its beauty but not everyone sees it.'
+Fortune.create :quotation => 'It does not matter how slowly you go so long as you do not stop.'
+Fortune.create :quotation => 'Study the past if you would define the future.'
