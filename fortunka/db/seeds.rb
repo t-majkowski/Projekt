f13446343563aa00b7e406981b9804933763c4f7
Tomasz Majkowski <majak_15@o2.pl>
2011-11-22 14:10:42 +0100

    nowe dane

--- fortunka/db/seeds.rb
+++ fortunka/db/seeds.rb
@@ -6,22 +6,6 @@ Fortune.create(quotation: 'Egoism is the very essence of a noble soul.' , source
 Fortune.create(quotation: 'Fear is the mother of morality.' , source: 'Friedrich Nietzsche')
 Fortune.create(quotation: 'Keep your eyes on the stars, and your feet on the ground.' , source: 'Theodore Roosevelt')
 Fortune.create(quotation: 'Just in terms of allocation of time resources, religion is not very efficient. There is a lot more I could be doing on a Sunday morning' , source: 'Bill Gates')
-
-
-
-=======
->>>>>>> 8d2aebde7e651eb71d84c9de057e64e9884be77f
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+Fortune.create(quotation: 'Keep your eyes on the stars, and your feet on the ground.' , source: 'Theodore Roosevelt')
 <<<<<<< HEAD
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-=======
-Fortune.create :quotation => 'I hear and i forget. I see and i remember. I do and I understand.'
-Fortune.create :quotation => 'Everything has its beauty but not everyone sees it.'
-Fortune.create :quotation => 'It does not matter how slowly you go so long as you do not stop.'
-Fortune.create :quotation => 'Study the past if you would define the future.'
->>>>>>> 8d2aebde7e651eb71d84c9de057e64e9884be77f
+
